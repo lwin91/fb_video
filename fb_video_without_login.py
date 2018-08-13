cd02252a05900caa84a590a971a0a19211372eef
lwin91 <lwin9161@gmail.com>
2018-08-13 14:20:46 +0400

    video details

---
+++ fb_video_without_login.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, WebDriverException, NoSuchElementException
+import selenium.webdriver.support.expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+
+
+options = Options()
+options.add_argument('--blink-settings=imagesEnabled=false')
+driver = webdriver.Chrome('C://chromedriver', chrome_options=options)
+
+
+def get_video_post_details(video_post):
+	driver.get(video_post)
+	views = driver.find_element_by_xpath('//span[@class="fcg"]').text.split()[0].replace('K', '000')
+	likes = driver.find_element_by_xpath('//span[contains(text(),"Likes")]').text.split()[0]
+	comments = driver.find_element_by_xpath('//span[contains(text(),"Comments")]').text.split()[0]
+	return [views, likes, comments]
+
+
+details = get_video_post_details('https://www.facebook.com/JungleVT/videos/1720510198003354/')
+print(details)
+driver.close()
